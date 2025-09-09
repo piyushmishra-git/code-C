e26af5ae1123f6b1f23e0fd36abb996e5554d4fa
piyushmishra-git <piyushmishra.github@gmail.com>
2025-09-09 15:12:53 +0530

    yes

--- Lecture 9/3.c
+++ Lecture 9/3.c
@@ -10,7 +10,7 @@ int main()
     {
         for (int j = 0; j <=i; j++)
         {
-            printf("%c ", j+65);
+            printf("%c ", j+97);
         }
         printf("\n");            
     }
