23b0690a6f6cb6776dc04f0994bf97776ea76f9b
piyushmishra-git <piyushmishra.github@gmail.com>
2025-10-13 10:53:53 +0530

    string

--- Lecture 22/sort_a_string.c
+++ Lecture 22/sort_a_string.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
 int main()
 {
-  char str="balllon";
-  int n=strlen(str);
+  char str[] = "balllon";
+  int n = strlen(str);
 
   for (int i = 0; i < n - 1; i++)
   {
     int swaps = 0;
     for (int j = 0; j < n - i - 1; j++)
     {
-      if ('str[j]' > 'str[j + 1]')
+      if (str[j] > str[j + 1])
       {
-        int temp = a[j + 1];
-        a[j + 1] = a[j];
-        a[j] = temp;
+        char temp = str[j + 1];
+        str[j + 1] = str[j];
+        str[j] = temp;
         swaps++;
       }
     }
     if (swaps == 0)
       break;
   }
+
   for (int i = 0; i < n; i++)
-    printf("%d ", a[i]);
+    printf("%c", str[i]);
 
   return 0;
 }
